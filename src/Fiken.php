b55baf43500aff2687df9a3b7d537b0dc084bd4c
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-03-06 13:06:47 +0100

    Rename class

--- src/Fiken.php
+++ src/Fiken.php
@@ -6,7 +6,7 @@ namespace FikenPhpSdk;
 use GuzzleHttp\Client as HttpClient;
 
 
-class Client
+class Fiken
 {
     protected $baseurl = 'https://fiken.no/api/v1';
     protected $client;
@@ -16,34 +16,9 @@ class Client
         $this->client = new HttpClient();
     }
 
-    public function createInvoice($options)
-    {
-        $uri = '/companies/' . $this->getCustomerId() . '/create-invoice-service';
-        $method = 'post';
-
-        return $this->sendRequest($method, $uri, $options);
-    }
-
-    public function getCustomerId()
-    {
-        $uri = '/companies';
-        $method = 'post';
-
-        $response = $this->sendRequest($method, $uri);
-
-        return $response;
-    }
-
     protected function sendRequest($method, $uri, $options)
     {
         return $this->client->request($method, $this->baseurl . $uri, $options);
     }
 
-    public function sendInvoice($options)
-    {
-        $uri = '/companies/' . $this->getCustomerId() . '/document-sending-service';
-        $method = 'post';
-
-        return $this->sendRequest($method, $uri, $options);
-    }
 }
