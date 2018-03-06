6e53d48293d6c98011ddb7c01f5ad56cd5dbaf8d
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-03-06 13:07:01 +0100

    Create invoice class

--- src/Invoice.php
+++ src/Invoice.php
@@ -11,5 +11,19 @@ namespace FikenPhpSdk;
 
 class Invoice
 {
+    public function sendInvoice($options)
+    {
+        $uri = '/companies/' . $this->getCustomerId() . '/document-sending-service';
+        $method = 'post';
 
+        return $this->sendRequest($method, $uri, $options);
+    }
+
+    public function createInvoice($options)
+    {
+        $uri = '/companies/' . $this->getCustomerId() . '/create-invoice-service';
+        $method = 'post';
+
+        return $this->sendRequest($method, $uri, $options);
+    }
 }
