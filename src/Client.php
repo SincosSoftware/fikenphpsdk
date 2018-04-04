dd9e906fbb3ff51b485e6b6749cd1ae17c4b8772
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-04-04 13:56:00 +0200

    Add support for sending requests with company slug

--- src/Client.php
+++ src/Client.php
@@ -2,23 +2,64 @@
 
 namespace FikenSDK;
 
-use GuzzleHttp\ClientInterface as HttpClientInterface;
+use Exception;
+use FikenSDK\Clients\ResourceClient;
+use GuzzleHttp\Client as HttpClient;
 
 class Client
 {
+    public $apiCredentials;
     protected $httpClient;
+    protected $company;
+    protected $resourceClients;
 
-    public function __construct(HttpClientInterface $httpClient)
+    public function __construct($username, $password, $company)
     {
-        $this->httpClient = $httpClient;
+        $this->httpClient = new HttpClient([
+            'auth' => [$username, $password],
+        ]);
+
+        $this->company = $company;
     }
 
     public function __get($argument)
     {
-        $class = 'FikenSDK\\Clients\\' . ucfirst($argument);
-
-        if (class_exists($class)) {
-            return new $class($this->httpClient);
+        if (! isset($this->resourceClients[$argument])) {
+            $this->resourceClients[$argument] = $this->createClient($argument);
         }
+
+        return $this->resourceClients[$argument];
+    }
+
+    /**
+     * @param $resourceName
+     * @return mixed
+     * @throws Exception
+     */
+    protected function createClient($resourceName)
+    {
+        $class = 'FikenSDK\\Clients\\' . ucfirst($resourceName);
+
+        if ($this->isValidClient($class)) {
+            return new $class($this->httpClient, $this->company);
+        }
+    }
+
+    /**
+     * @param $class
+     * @return bool
+     * @throws Exception
+     */
+    protected function isValidClient($class)
+    {
+        if (! class_exists($class)) {
+            throw new Exception('Class ' . $class . ' does not exist.');
+        }
+
+        if (! is_subclass_of($class, ResourceClient::class)) {
+            throw new Exception('Class ' . $class . ' doesn\'t implement ' . ResourceClient::class . '.');
+        }
+
+        return true;
     }
 }
