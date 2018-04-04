49049c3926405003910bc95da3e9c2348124a7af
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-04-04 15:38:48 +0200

    Remove test

--- src/Clients/WhoAmI.php
+++ src/Clients/WhoAmI.php
@@ -2,19 +2,10 @@
 
 namespace FikenSDK\Clients;
 
-use GuzzleHttp\Client;
-
 class WhoAmI extends ResourceClient
 {
-    public function __construct(Client $httpClient, $companySlug = null)
-    {
-        parent::__construct($httpClient, $companySlug);
-    }
-
     public function whoAmI()
     {
-
-
         $response = $this->httpClient->get(self::BASE_URL . '/whoAmI');
 
         return $this->responseToArray($response);
