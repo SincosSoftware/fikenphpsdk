edff23c85d5f7e1244141dbf518f0ed3a61e62c1
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-04-04 15:38:07 +0200

    Work in progress

--- src/Clients/Customer.php
+++ src/Clients/Customer.php
@@ -2,7 +2,27 @@
 
 namespace FikenSDK\Clients;
 
-class Customer
+class Customer extends ResourceClient
 {
+    public function test(){
+        return $this->findLink('contacts');
+    }
 
+    public function getCustomer($customer){
+        if ($this->customerExistsInFiken()){
+            return $customer;
+        }
+
+        return $this->createCustomer();
+    }
+
+    protected function createCustomer()
+    {
+        $this->httpClient->post($this->findLink('contacts'));
+    }
+
+    protected function customerExistsInFiken()
+    {
+        $response = $this->httpClient->get(self::BASE_URL . '' . $this->findLink('contacts'));
+    }
 }
--- src/Clients/ResourceClient.php
+++ src/Clients/ResourceClient.php
@@ -2,7 +2,45 @@
 
 namespace FikenSDK\Clients;
 
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
 abstract class ResourceClient
 {
     const BASE_URL = 'https://fiken.no/api/v1';
+    /**
+     * @var Client
+     */
+    protected $httpClient;
+    /**
+     * @var string|null
+     */
+    protected $companySlug;
+
+    public function __construct(Client $httpClient, $companySlug = null)
+    {
+        $this->httpClient = $httpClient;
+        $this->companySlug = $companySlug;
+    }
+
+    protected function responseToArray(ResponseInterface $response)
+    {
+        return json_decode($response->getBody(), true);
+    }
+
+    public function getLinks()
+    {
+        $response = $this->httpClient->get(self::BASE_URL);
+
+        return $this->responseToArray($response);
+    }
+
+    public function findLink($needle)
+    {
+        foreach ($this->getLinks()['_embedded'] as $companies){
+            if ($companies['slug'] == $this->companySlug) {
+            }
+        }
+        return $relLink = self::BASE_URL . '/rel/' . $needle;
+    }
 }
