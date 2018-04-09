9c087c67c53e7b858ddd0f07ba0e5e1eb5a98388
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-04-09 08:59:51 +0200

    Add new exception and improve old exception

--- src/Exceptions/InvalidTypeException.php
+++ src/Exceptions/InvalidTypeException.php
@@ -2,12 +2,17 @@
 
 namespace FikenSDK\Exceptions;
 
+use Closure;
 use UnexpectedValueException;
 
 class InvalidTypeException extends UnexpectedValueException
 {
     public function __construct($name, $type)
     {
+        if ($type instanceof Closure) {
+            return parent::__construct('Property ' . $name . ' does not conform to requirements.');
+        }
+
         parent::__construct('Property ' . $name . ' must be of type ' . $type . '.');
     }
 }
---
+++ src/Exceptions/MissingRequiredPropertyException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FikenSDK\Exceptions;
+
+use Exception;
+
+class MissingRequiredPropertyException extends Exception
+{
+    public function __construct($class, array $properties)
+    {
+        parent::__construct(
+            'The class ' . $class . ' is missing the following required properties: '
+            . implode(', ', $properties) . '.'
+        );
+    }
+}
