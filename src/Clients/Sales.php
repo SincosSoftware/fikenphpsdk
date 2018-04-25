9175f109fd44f7d922d98b0e3d1fa11e41ca519d
Åsbjørn Hansen <asbjorn@24nettbutikk.no>
2018-04-25 14:21:25 +0200

    Remove DD

--- src/Clients/Sales.php
+++ src/Clients/Sales.php
@@ -8,7 +8,6 @@ class Sales extends ResourceClient
 {
     public function createSale(Sale $sale)
     {
-        //dd(json_encode($sale->toArray()));
         return $this->post($sale->toArray());
     }
 }
