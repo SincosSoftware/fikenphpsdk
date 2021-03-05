825ab33506cdf44334e1deac594daafba3a83b62
Even Eriksen <eveneriksen@Evens-MacBook-Pro.local>
2021-03-05 13:25:05 +0100

    Use correct constontant name

--- src/Configurations/Accounts.php
+++ src/Configurations/Accounts.php
@@ -6,7 +6,7 @@ class Accounts
 {
     const SALES_HIGH_VAT = '3000';
     const SALES_MEDIUM_VAT = '3030';
-    const SALES_EX_VAT = '3100';
+    const SALES_EXEMPT_VAT = '3100';
     const SHIPPING = '6940';
     const SALES_COSTS = '7300';
     const GIFTCARDS = '2900';
